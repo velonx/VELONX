b407387f79e905955c75013ee0c98bdda9d0c312
Rishi pandey <148840129+rishipandey9399@users.noreply.github.com>
2026-02-07 16:22:24 +0530

    Refactor ESLint config to use array syntax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
-export default defineConfig({
-  ...nextVitals,
-  ...nextTs,
-  ignores: [
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ],
-});
+export default defineConfig([
+  nextVitals,
+  nextTs,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+]);
