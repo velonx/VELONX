64f1da83c910e63474257c2044c43183ae78e4d3
rishipandey9399 <grearrishi@gmail.com>
2026-05-09 01:35:12 +0530

    chore: remove deprecated trustHost configuration from NextAuth setup

--- src/auth.ts
+++ src/auth.ts
@@ -45,7 +45,6 @@ declare module "@auth/core/jwt" {
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    trustHost: true,
     adapter: {
         ...PrismaAdapter(prisma),
         createUser: async (user) => {
